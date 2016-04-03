519036968fe03efa034127c8e1bb61fe64ae4ae0
addb <arjun.d.bhadra@gmail.com>
2016-04-03 13:53:34 -0700

    edited getResult file
    edited getResult file

--- Frontend/getResult.php
+++ Frontend/getResult.php
@@ -1,8 +1,8 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "addb";
-	$dbname = "ir_db";
+	$servername = "";
+	$username = "";
+	$password = "";
+	$dbname = "";
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
